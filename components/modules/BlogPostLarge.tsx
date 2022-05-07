6f350279b73ea629f92bf5815868a45fd62fb287
daniel planötscher <dplanoetscher@unibz.it>
2022-05-07 20:38:58 +0200

    blog styling

--- components/modules/BlogPostLarge.tsx
+++ components/modules/BlogPostLarge.tsx
@@ -1,39 +1,37 @@
 import styles from "@styles/modules/BlogPost.module.scss";
-import SlideIn from "@components/animations/SlideIn";
-import { Post } from "@prisma/client";
+import {Post} from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-  post: Post;
+    post: Post;
 }
 
-const BlogPostLarge = ({ post }: Props): JSX.Element => {
-  return (
-    <section className={styles.module}>
-      <div className="container">
-        <Link href={`/blog/${post.slug}`}>
-          <a className={styles.inner + " clickable"}>
-            <div className={styles.image}>
-              <Image
-                  src={post.preview_image}
-                  alt=""
-                  objectFit="cover"
-                  layout="fill"
-              />
+const BlogPostLarge = ({post}: Props): JSX.Element => {
+    return (
+        <section className={styles.module + " " + styles.large}>
+            <div className="container">
+                <Link href={`/blog/${post.slug}`}>
+                    <a className={styles.inner + " clickable"}>
+                        <div className={styles.image}>
+                            <Image
+                                src={post.preview_image}
+                                alt=""
+                                objectFit="cover"
+                                layout="fill"
+                            />
+                        </div>
+                        <div className={styles.content}>
+                            <h2>{post.name}</h2>
+                            <div className={styles.text}>
+                                {post.lead.substring(0, 200)}...
+                            </div>
+                        </div>
+                    </a>
+                </Link>
             </div>
-            <div className={styles.content}>
-                <h2>{post.name}</h2>
-              <div className={styles.text}>
-                {post.lead.substring(0, 200)}...
-
-              </div>
-            </div>
-          </a>
-        </Link>
-      </div>
-    </section>
-  );
+        </section>
+    );
 };
 
 export default BlogPostLarge;
--- components/modules/BlogPostSmall.tsx
+++ components/modules/BlogPostSmall.tsx
@@ -1,39 +1,36 @@
 import styles from "@styles/modules/BlogPost.module.scss";
-import { Post } from "@prisma/client";
+import {Post} from "@prisma/client";
 import Image from "next/image";
 import Link from "next/link";
 
 interface Props {
-  post: Post;
+    post: Post;
 }
 
-const BlogPostSmall = ({ post }: Props): JSX.Element => {
-  return (
-    <section className={styles.module}>
-      <div className="container">
-        <Link href={`/blog/${post.slug}`}>
-          <a className={`${styles["small-inner"]} clickable`}>
-            <div className={styles.content}>
-              <h2>{post.name}</h2>
-            </div>
-            <div className={styles.top}>
-              <div className={styles.year}>
-                {new Date(post.create_date).getFullYear()}
-              </div>
-            </div>
-            <div className={styles.background}>
-              <Image
-                src={post.preview_image}
-                alt=""
-                objectFit="cover"
-                layout="fill"
-              />
-            </div>
-          </a>
-        </Link>
-      </div>
-    </section>
-  );
+const BlogPostSmall = ({post}: Props): JSX.Element => {
+    const date = new Date(post.create_date);
+    return (
+        <section className={styles.module + " " + styles.small}>
+            <Link href={`/blog/${post.slug}`}>
+                <a className={`${styles["small-inner"]} clickable`}>
+                    <div className={styles.image}>
+                        <Image
+                            src={post.preview_image}
+                            alt=""
+                            objectFit="cover"
+                            layout="fill"
+                        />
+                    </div>
+                    <div className={styles.content}>
+                        <h2>{post.name}</h2>
+                    </div>
+                    <div className={styles.year}>
+                        {date.getDate()}/{date.getMonth()}/{date.getFullYear()}
+                    </div>
+                </a>
+            </Link>
+        </section>
+    );
 };
 
 export default BlogPostSmall;
