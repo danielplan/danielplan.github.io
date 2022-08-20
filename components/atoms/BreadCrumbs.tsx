b5caea91fd56f9e0fdb1a075be92f5c4bd126d0b
daniel planötscher <dplanoetscher@unibz.it>
2022-08-20 15:00:32 +0200

    rm console.log

--- components/atoms/BreadCrumbs.tsx
+++ components/atoms/BreadCrumbs.tsx
@@ -8,22 +8,6 @@ interface Props {
 }
 
 const BreadCrumbs = ({breadcrumbs}: Props): JSX.Element => {
-  console.log(
-    JSON.stringify({
-      '@context': 'http://schema.org',
-      '@type': 'BreadcrumbList',
-      itemListElement: breadcrumbs.map((breadcrumb, i) => ({
-        '@type': 'ListItem',
-        position: i + 1,
-
-        item: {
-          '@id': BASE_URL + breadcrumb.path,
-          name: breadcrumb.name,
-        },
-      })),
-    })
-  );
-
   return (
     <script type="application/ld+json">
       {JSON.stringify({
